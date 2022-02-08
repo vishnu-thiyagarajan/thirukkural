b806ed059c25d9a1df0731269e84f48414d195f7
vishnu-thiyagarajan <vishnu.thg@gmail.com>
2022-02-08 23:43:43 +0530

    order imports

--- src/components/Kural.js
+++ src/components/Kural.js
@@ -1,9 +1,9 @@
-import React from "react";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
+import Typography from "@mui/material/Typography";
+import React from "react";
 
 const options = {
   mv: "மு.வரதராசன்",
